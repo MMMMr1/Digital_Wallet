69816739e715491e993019806e25a5ebaa232161
Michalenok Marina <mihalenokee@outlook.com>
2023-10-22 21:06:38 +0300

    feat: WalletUserServiceRunner, add to docker-compose  "platform: linux/amd64"

---
+++ user-service/src/main/java/com/michalenok/wallet/WalletUserServiceRunner.java
@@ -0,0 +1,11 @@
+package com.michalenok.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletUserServiceRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(WalletUserServiceRunner.class, args);
+    }
+}
