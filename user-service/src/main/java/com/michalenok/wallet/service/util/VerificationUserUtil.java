c9039b3b201fea7f51257e3d03bcc8e3c8b2f7b9
Michalenok Marina <mihalenokee@outlook.com>
2023-10-30 21:13:19 +0300

    feat: util classes (CreationUserUtil, VerificationUserUtil)

---
+++ user-service/src/main/java/com/michalenok/wallet/service/util/VerificationUserUtil.java
@@ -0,0 +1,11 @@
+package com.michalenok.wallet.service.util;
+
+import org.springframework.stereotype.Component;
+import java.util.UUID;
+
+@Component
+public class VerificationUserUtil {
+    public String generateCode(){
+        return UUID.randomUUID().toString();
+    }
+}
