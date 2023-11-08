4494a331560c357008f2b994f276b2bd2f88588f
Michalenok Marina <mihalenokee@outlook.com>
2023-11-08 17:39:49 +0300

    fix: remove swagger to api gateway

--- api-gateway-server/src/main/java/com/michalenok/wallet/WalletApiGatewayServerApplication.java
+++ api-gateway-server/src/main/java/com/michalenok/wallet/WalletApiGatewayServerApplication.java
@@ -1,10 +1,13 @@
 package com.michalenok.wallet;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @RefreshScope
+@OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "Documentation API Gateway v1.0"))
 @SpringBootApplication
 public class WalletApiGatewayServerApplication {
 
--- user-service/src/main/java/com/michalenok/wallet/configuration/SwaggerConfiguration.java
+++ user-service/src/main/java/com/michalenok/wallet/configuration/SwaggerConfiguration.java
@@ -1,16 +1,23 @@
 package com.michalenok.wallet.configuration;
 
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition
 @Configuration
 public class SwaggerConfiguration {
     @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("user-service")
-                .pathsToMatch("/api/v1/users/**")
-                .build();
+    public OpenAPI customOpenApi(
+            @Value("${openapi.service.name}") String appName,
+            @Value("${openapi.service.version}") String serviceVersion,
+            @Value("${openapi.service.description}") String appDescription) {
+        return new OpenAPI()
+                .info(new Info().title(appName)
+                        .version(serviceVersion)
+                        .description(appDescription));
     }
 }
