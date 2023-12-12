88abcd202bbdb5de541bb429711d5d0450871ed9
Michalenok Marina <mihalenokee@outlook.com>
2023-12-12 14:10:33 +0300

    Jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,19 +4,9 @@ pipeline {
         gradle '8.5'
     }
     stages {
-        stage('Clean') {
-            steps {
-                 sh 'gradle clean'
-            }
-        }
-        stage('Test') {
-            steps {
-                 sh 'gradle :user-service:test'
-            }
-        }
         stage('Build') {
             steps {
-                 sh 'gradle build'
+                 sh 'gradle clean build'
             }
         }
     }
--- user-service/src/test/java/com/michalenok/wallet/integration/IntegrationTestBase.java
+++ user-service/src/test/java/com/michalenok/wallet/integration/IntegrationTestBase.java
@@ -1,6 +1,5 @@
 package com.michalenok.wallet.integration;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
@@ -9,27 +8,23 @@ import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.utility.DockerImageName;
-
+import org.testcontainers.junit.jupiter.Testcontainers; ;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 
 @ActiveProfiles("test")
-@SpringBootTest
+@Testcontainers
 @Transactional
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @SqlGroup({
         @Sql(scripts = "classpath:sql/data.sql", executionPhase = BEFORE_TEST_METHOD),
         @Sql(scripts = "classpath:sql/clear_data.sql", executionPhase = AFTER_TEST_METHOD)})
 public abstract class IntegrationTestBase {
+
     @Container
     @ServiceConnection
-    static PostgreSQLContainer postgres = new PostgreSQLContainer(DockerImageName.parse("postgres"))
+    static PostgreSQLContainer postgres = new PostgreSQLContainer( "postgres:15.2-alpine" )
             .withDatabaseName("postgres")
             .withUsername("postgres")
             .withPassword("root");
-
-    @BeforeAll
-    static void runContainer(){
-        postgres.start();
-    }
 }
