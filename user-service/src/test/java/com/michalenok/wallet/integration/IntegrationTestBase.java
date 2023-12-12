aee0a1150f7591cbb8455b86855d1842a054d259
Michalenok Marina <mihalenokee@outlook.com>
2023-12-12 15:12:31 +0300

    delete integration tests

--- user-service/src/test/java/com/michalenok/wallet/integration/IntegrationTestBase.java
+++ user-service/src/test/java/com/michalenok/wallet/integration/IntegrationTestBase.java
@@ -1,30 +1,30 @@
-package com.michalenok.wallet.integration;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlGroup;
-import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers; ;
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
-
-@ActiveProfiles("test")
-@Testcontainers
-@Transactional
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@SqlGroup({
-        @Sql(scripts = "classpath:sql/data.sql", executionPhase = BEFORE_TEST_METHOD),
-        @Sql(scripts = "classpath:sql/clear_data.sql", executionPhase = AFTER_TEST_METHOD)})
-public abstract class IntegrationTestBase {
-
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer postgres = new PostgreSQLContainer( "postgres:15.2-alpine" )
-            .withDatabaseName("postgres")
-            .withUsername("postgres")
-            .withPassword("root");
-}
+//package com.michalenok.wallet.integration;
+//
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.jdbc.Sql;
+//import org.springframework.test.context.jdbc.SqlGroup;
+//import org.springframework.transaction.annotation.Transactional;
+//import org.testcontainers.containers.PostgreSQLContainer;
+//import org.testcontainers.junit.jupiter.Container;
+//import org.testcontainers.junit.jupiter.Testcontainers; ;
+//import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+//import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+//
+//@ActiveProfiles("test")
+//@Testcontainers
+//@Transactional
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@SqlGroup({
+//        @Sql(scripts = "classpath:sql/data.sql", executionPhase = BEFORE_TEST_METHOD),
+//        @Sql(scripts = "classpath:sql/clear_data.sql", executionPhase = AFTER_TEST_METHOD)})
+//public abstract class IntegrationTestBase {
+//
+//    @Container
+//    @ServiceConnection
+//    static PostgreSQLContainer postgres = new PostgreSQLContainer( "postgres:15.2-alpine" )
+//            .withDatabaseName("postgres")
+//            .withUsername("postgres")
+//            .withPassword("root");
+//}
